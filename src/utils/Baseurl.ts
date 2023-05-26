0b064d1fa9812ccd23efafcd6efcbe46ebf448d9
Esther <ejidikeesther@gmail.com>
2023-05-26 14:24:04 +0100

    Change base url

--- src/utils/Baseurl.ts
+++ src/utils/Baseurl.ts
@@ -1,2 +1,2 @@
-export const Baseurl = import.meta.env.DEV ? 'http://localhost:8080/api/v1' : 'https://anonbackend.cyclic.app/api/v1'
+export const Baseurl = import.meta.env.DEV ? 'http://localhost:8080/api/v1' : 'https://anon-backend.onrender.com/api/v1'
 //export const Baseurl = 'https://anonbackend.cyclic.app/api/v1'
